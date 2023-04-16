43195c0173602b792dd6adcba8a784b03e8a38d2
wmastalerz <wmastalerz@tlen.pl>
2023-04-16 20:01:07 +0200

    add load balancer metadata

--- kind-loadbalancer.sh
+++ kind-loadbalancer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+kubectl apply -f metallb-native.yaml
 kubectl wait --namespace metallb-system \
                 --for=condition=ready pod \
                 --selector=app=metallb \
@@ -7,8 +7,7 @@ kubectl wait --namespace metallb-system \
 
 ippool=$(docker network inspect -f '{{.IPAM.Config}}' kind)
 ipprefix="${ippool:2:7}"
-kubectl apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-config.yaml
-wget  https://kind.sigs.k8s.io/examples/loadbalancer/metallb-config.yaml
+kubectl apply -f metallb-config.yaml
 metalb=$(cat metallb-config.yaml)
 echo "${metalb//172.19./$ipprefix}" | kubectl apply -f -
 
